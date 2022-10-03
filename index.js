09244885e981f5da0333c7c8a689acb3013d2789
Anand-Stark <starkyam32@gmail.com>
2022-10-04 02:27:08 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function retnumber(){
+    var x=Math.floor(Math.random()*6)+1;
+
+    return x;
+}
+
+var randomNumber1=retnumber();
+var randomImageSource1="images/dice"+randomNumber1 +".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1 );
+
+var randomNumber2=retnumber();
+var randomImageSource2="images/dice"+randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",  randomImageSource2 );
+
+if(randomNumber1>randomNumber2)
+{
+   document.querySelector("h1").innerHTML="❤️ Player 1 wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins ❤️";
+}
+else{
+    document.querySelector("h1").innerHTML="❤️ Its a tie ❤️";
+}
